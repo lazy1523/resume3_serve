d0bb66fdf1ffacd3a38cb4ddffdc9dded147468b
zisu <lazy1523@outlook.com>
2023-07-04 22:44:06 -0300

    update

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -4,7 +4,7 @@ import { HomeService } from './home.service';
 import { ApiResult } from 'src/support/code/ApiResult';
 
 @ApiTags('Home')
-@Controller({path: 'home',version: '1'})
+@Controller({path: '/',version: '1'})
 export class HomeController {
   private logger: Logger = new Logger(HomeController.name);
   constructor(private service: HomeService) { }
