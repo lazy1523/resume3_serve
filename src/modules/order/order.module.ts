0fde5d5ce05c385e574db987d1ec3d592570e497
zisu <lazy1523@outlook.com>
2023-07-07 11:21:25 +0800

    update fix error

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,10 +4,11 @@ import { OrderSchema } from 'src/models/order';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { BlockchainModule } from 'src/support/blockchain/blockchain.module';
+import { AccountSchema } from 'src/models/account';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Order', schema: OrderSchema }]),
+    MongooseModule.forFeature([{ name: 'Order', schema: OrderSchema },  { name: 'Account', schema:AccountSchema },]),
     BlockchainModule
   ],
   controllers: [OrderController],
